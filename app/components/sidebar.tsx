591e2dc6e4e6ef7236f7aee6fd0660dd6da4d1a6
吃草莓的小屋 <62467038+chrelyonly@users.noreply.github.com>
2023-04-24 18:52:12 +0800

    111111

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -111,24 +111,6 @@ export function SideBar(props: { className?: string }) {
       </div>
 
       <div className={styles["sidebar-tail"]}>
-        {/* <div className={styles["sidebar-actions"]}>
-          <div className={styles["sidebar-action"] + " " + styles.mobile}>
-            <IconButton
-              icon={<CloseIcon />}
-              onClick={chatStore.deleteSession}
-            />
-          </div>
-          <div className={styles["sidebar-action"]} style={{display:none}}>
-            <Link to={Path.Settings}>
-              <IconButton icon={<SettingsIcon />} shadow />
-            </Link>
-          </div>
-          <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
-          </div>
-        </div> */}
         <div>
           <IconButton
             icon={<AddIcon />}
